87ddbda2e1a2d3c89621e69da843a804de1e2564
LANE Boys RC <laneboysrc@gmail.com>
2016-07-12 21:29:21 +0800

    Refactor component architecture

--- configurator/_includes/mixer.js
+++ configurator/_includes/mixer.js
@@ -1,42 +1,44 @@
-/*jslint browser: true */
-/*global Path, Device, showPage */
-"use strict";
+(function () {
+    'use strict';
 
-var Mixer = {
-    uuid: null,
+    var Mixer = function Mixer() {
+        this.uuid = null;
 
-    offset: 0,
-    setTextContent: MDLHelper.setTextContent,
-    setSwitch: MDLHelper.setSwitch,
+        this.offset = 0;
+        this.setTextContent = MDLHelper.setTextContent;
+        this.setSwitch = MDLHelper.setSwitch;
+    };
+    window['Mixer'] = new Mixer();
 
-    init: function (params) {
+    //*************************************************************************
+    Mixer.prototype.init = function (params) {
         this.uuid = params.model_uuid;
 
-        let config = Database.getConfig(this.uuid);
-        let types = config.TYPES;
-        let src_choices = types[config.MODEL.MIXER_UNITS_SRC.t];
-        let unused = typeLookupByNumber(src_choices, 0);
-        let mixer_list = document.querySelector('#app-mixer-list');
+        var config = Database.getConfig(this.uuid);
+        var types = config.TYPES;
+        var src_choices = types[config.MODEL.MIXER_UNITS_SRC.t];
+        var unused = typeLookupByNumber(src_choices, 0);
+        var mixer_list = document.querySelector('#app-mixer-list');
 
         // Clear the app-mixer-list based on the class we've added when
         // instantiating the template
         while (mixer_list.querySelector('.can-delete')) {
-            let elem = mixer_list.querySelector('.can-delete');
+            var elem = mixer_list.querySelector('.can-delete');
             elem.parentNode.removeChild(elem);
         }
 
-        let t = document.querySelector('#app-mixer-template');
+        var t = document.querySelector('#app-mixer-template');
 
-        for (let i = 0; i < config.MODEL.MIXER_UNITS.c; i++) {
-            let offset = i * config.MODEL.MIXER_UNITS.s;
-            let src = Database.get(this.uuid, 'MIXER_UNITS_SRC', offset);
+        for (var i = 0; i < config.MODEL.MIXER_UNITS.c; i++) {
+            var offset = i * config.MODEL.MIXER_UNITS.s;
+            var src = Database.get(this.uuid, 'MIXER_UNITS_SRC', offset);
             if (src === unused) {
                 break;
             }
 
-            let dst = Database.get(this.uuid, 'MIXER_UNITS_DST', offset);
-            let curve_type = Database.get(this.uuid, 'MIXER_UNITS_CURVE_TYPE', offset);
-            let op = Database.get(this.uuid, 'MIXER_UNITS_OP', offset);
+            var dst = Database.get(this.uuid, 'MIXER_UNITS_DST', offset);
+            var curve_type = Database.get(this.uuid, 'MIXER_UNITS_CURVE_TYPE', offset);
+            var op = Database.get(this.uuid, 'MIXER_UNITS_OP', offset);
 
             t.content.querySelector('tr').classList.add('can-delete');
             t.content.querySelector('#app-mixer-template-src').textContent = src;
@@ -44,14 +46,15 @@ var Mixer = {
             t.content.querySelector('#app-mixer-template-mixer_unit').setAttribute('data-url', '#/mixer_unit/' + this.uuid + '/' + i);
             t.content.querySelector('#app-mixer-template-dst').textContent = dst;
             t.content.querySelector('#app-mixer-template-dst').setAttribute('data-url', '#/limits/' + this.uuid + '/' + dst);
-            let clone = document.importNode(t.content, true);
+            var clone = document.importNode(t.content, true);
             mixer_list.appendChild(clone);
         }
-    },
+    };
+})();
 
-    route: function () {
-        Mixer.init(this.params);
-        showPage('mixer');
-    }
+
+Mixer.route = function () {
+    Mixer.init(this.params);
+    showPage('mixer');
 };
 

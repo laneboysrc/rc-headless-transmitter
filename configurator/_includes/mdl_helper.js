85187014f6ed1f2419a69b4d1e40c7594d0d97ed
Werner Lane <laneboysrc@gmail.com>
2016-07-13 09:35:46 +0800

    Refactoring to make MDLHelper a class we instantiate

--- configurator/_includes/mdl_helper.js
+++ configurator/_includes/mdl_helper.js
@@ -1,39 +1,60 @@
-"use strict";
+(function () {
+    'use strict';
+    var MDLHelper = function MDLHelper(uuid, offset=0, onChangeHandler=undefined) {
+        this.uuid = uuid;
+        this.offset = offset;
+        this.onChangeHandler = onChangeHandler;
+    };
 
-var MDLHelper = {
-    setTextContent: function (selector, item) {
-        let value = Database.get(this.uuid, item, this.offset);
-        document.querySelector(selector).textContent = value;
-    },
+    window['MDLHelper'] = MDLHelper;
 
-    setSwitch: function (selector, item) {
-        let value = Database.get(this.uuid, item, this.offset);
-        let element = document.querySelector(selector);
+    MDLHelper.prototype.setTextContent = function (selector, item, root=document) {
+        var value = Database.get(this.uuid, item, this.offset);
+        root.querySelector(selector).textContent = value;
+    };
+
+    MDLHelper.prototype.setTextContentRaw = function (selector, value, root=document) {
+        root.querySelector(selector).textContent = value;
+    };
+
+    MDLHelper.prototype.setSwitch = function (selector, item, root=document) {
+        var value = Database.get(this.uuid, item, this.offset);
+        var element = root.querySelector(selector);
         element.checked = value;
         element.parentNode.MaterialSwitch.checkToggleState();
         element.setAttribute('data-source', item);
         element.onchange = this.onChangeHandler;
-    },
+    };
 
-   setSlider: function (selector, item) {
-        let value = Database.get(this.uuid, item, this.offset);
-        let element = document.querySelector(selector);
+   MDLHelper.prototype.setSlider = function (selector, item, root=document) {
+        var value = Database.get(this.uuid, item, this.offset);
+        var element = root.querySelector(selector);
         element.MaterialSlider.change(value);
         element.setAttribute('data-source', item);
         element.onchange = this.onChangeHandler;
-    },
+    };
 
-    setTextfield: function (selector, item) {
-        let value = Database.get(this.uuid, item, this.offset);
-        let element = document.querySelector(selector);
+    MDLHelper.prototype.setTextfield = function (selector, item, root=document) {
+        var value = Database.get(this.uuid, item, this.offset);
+        var element = root.querySelector(selector);
         element.value = value;
         element.setAttribute('data-source', item);
         element.onblur = this.onChangeHandler;
-    },
+    };
 
-    setDataURL: function (selector, list) {
-        let element = document.querySelector(selector);
-        let url = '#/' + list.join('/');
+    MDLHelper.prototype.setDataURL = function (selector, list, root=document) {
+        var element = root.querySelector(selector);
+        var url = '#/' + list.join('/');
         element.setAttribute('data-url', url);
-    }
-};
+    };
+
+    MDLHelper.prototype.clearDynamicElements = function (element) {
+        var child = element.querySelector('.can-delete');
+
+        while (child) {
+            child.parentNode.removeChild(child);
+            child = element.querySelector('.can-delete');
+        }
+    };
+})();
+
--- configurator/_includes/mixer.js
+++ configurator/_includes/mixer.js
@@ -4,9 +4,8 @@
     var Mixer = function Mixer() {
         this.uuid = null;
 
-        this.offset = 0;
-        this.setTextContent = MDLHelper.setTextContent;
-        this.setSwitch = MDLHelper.setSwitch;
+        this.template = document.querySelector('#app-mixer-template').content;
+        this.mixer_list = document.querySelector('#app-mixer-list');
     };
     window['Mixer'] = new Mixer();
 
@@ -14,40 +13,37 @@
     Mixer.prototype.init = function (params) {
         this.uuid = params.model_uuid;
 
-        var config = Database.getConfig(this.uuid);
-        var types = config.TYPES;
-        var src_choices = types[config.MODEL.MIXER_UNITS_SRC.t];
-        var unused = typeLookupByNumber(src_choices, 0);
-        var mixer_list = document.querySelector('#app-mixer-list');
+        var mdl = new MDLHelper(this.uuid);
+        var mixer_units = Database.getSchema(this.uuid)['MIXER_UNITS'];
 
-        // Clear the app-mixer-list based on the class we've added when
-        // instantiating the template
-        while (mixer_list.querySelector('.can-delete')) {
-            var elem = mixer_list.querySelector('.can-delete');
-            elem.parentNode.removeChild(elem);
-        }
+        // Empty the list of mixers
+        mdl.clearDynamicElements(this.mixer_list);
 
-        var t = document.querySelector('#app-mixer-template');
-
-        for (var i = 0; i < config.MODEL.MIXER_UNITS.c; i++) {
-            var offset = i * config.MODEL.MIXER_UNITS.s;
+        for (var i = 0; i < mixer_units.c; i++) {
+            var offset = i * mixer_units.s;
             var src = Database.get(this.uuid, 'MIXER_UNITS_SRC', offset);
-            if (src === unused) {
+
+            // End-of-list is indicated by the mixer unit source being 0
+            if (src === 0) {
                 break;
             }
 
-            var dst = Database.get(this.uuid, 'MIXER_UNITS_DST', offset);
+            mdl.offset = offset;
             var curve_type = Database.get(this.uuid, 'MIXER_UNITS_CURVE_TYPE', offset);
             var op = Database.get(this.uuid, 'MIXER_UNITS_OP', offset);
+            var curve = curve_type + ' ' + op;
+            var dst = Database.get(this.uuid, 'MIXER_UNITS_DST', offset);
 
-            t.content.querySelector('tr').classList.add('can-delete');
-            t.content.querySelector('#app-mixer-template-src').textContent = src;
-            t.content.querySelector('#app-mixer-template-mixer_unit').textContent = curve_type + ' ' + op;
-            t.content.querySelector('#app-mixer-template-mixer_unit').setAttribute('data-url', '#/mixer_unit/' + this.uuid + '/' + i);
-            t.content.querySelector('#app-mixer-template-dst').textContent = dst;
-            t.content.querySelector('#app-mixer-template-dst').setAttribute('data-url', '#/limits/' + this.uuid + '/' + dst);
-            var clone = document.importNode(t.content, true);
-            mixer_list.appendChild(clone);
+            var t = this.template;
+            t.querySelector('tr').classList.add('can-delete');
+            mdl.setTextContent('#app-mixer-template-src', 'MIXER_UNITS_SRC', t);
+            mdl.setTextContent('#app-mixer-template-dst', 'MIXER_UNITS_DST', t);
+            mdl.setTextContentRaw('#app-mixer-template-mixer_unit', curve, t);
+            mdl.setDataURL('#app-mixer-template-mixer_unit', ['mixer_unit', this.uuid, i], t);
+            mdl.setDataURL('#app-mixer-template-dst', ['limits', this.uuid, dst], t);
+
+            var clone = document.importNode(t, true);
+            this.mixer_list.appendChild(clone);
         }
     };
 })();

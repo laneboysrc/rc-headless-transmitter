658863e4495b1912ea6253f97848e3dfa1913696
Werner Lane <laneboysrc@gmail.com>
2016-07-13 11:27:06 +0800

    Refactor mixer_unit

--- configurator/_includes/mixer_unit.js
+++ configurator/_includes/mixer_unit.js
@@ -1,48 +1,42 @@
-/*jslint browser: true */
-/*global Path, Device, showPage */
-"use strict";
+(function () {
+    'use strict';
 
-var MixerUnit = {
-    uuid: undefined,
-    index: undefined,
+    var MixerUnit = function MixerUnit() {
+        this.uuid = undefined;
+        this.index = undefined;
+        this.offset = 0;
+    };
+    window['MixerUnit'] = new MixerUnit();
 
-    offset: 0,
-    setTextContent: MDLHelper.setTextContent,
-    setSwitch: MDLHelper.setSwitch,
-
-    onChangeHandler: function (event) {
-        let element = event.target;
-        let item = element.getAttribute('data-source');
-        let value = element.checked ? 1 : 0;
-
-        Database.set(MixerUnit.uuid, item, value, MixerUnit.offset);
-    },
-
-    init: function (params) {
+    //*************************************************************************
+    MixerUnit.prototype.init = function (params) {
         this.uuid = params.model_uuid;
         this.index = params.index;
 
-        let config = Database.getConfig(this.uuid);
-        this.offset = config.MODEL.MIXER_UNITS.s * this.index;
+        var mixer_units = Database.getSchema(this.uuid)['MIXER_UNITS'];
+        this.offset = mixer_units.s * this.index;
 
-        this.setTextContent('#app-mixer_unit-src', 'MIXER_UNITS_SRC')
-        this.setSwitch('#app-mixer_unit-invert_source', 'MIXER_UNITS_INVERT_SOURCE');
-        this.setTextContent('#app-mixer_unit-dst', 'MIXER_UNITS_DST')
-        this.setTextContent('#app-mixer_unit-sw', 'MIXER_UNITS_SW_SW')
-        this.setTextContent('#app-mixer_unit-op', 'MIXER_UNITS_OP')
-        this.setSwitch('#app-mixer_unit-apply_trim', 'MIXER_UNITS_APPLY_TRIM');
+        var mdl = new MDLHelper(this.uuid, this.offset);
 
-        MDLHelper.setDataURL('#app-mixer_unit-src__edit',
+        mdl.setTextContent('#app-mixer_unit-src', 'MIXER_UNITS_SRC');
+        mdl.setSwitch('#app-mixer_unit-invert_source', 'MIXER_UNITS_INVERT_SOURCE');
+        mdl.setTextContent('#app-mixer_unit-dst', 'MIXER_UNITS_DST');
+        mdl.setTextContent('#app-mixer_unit-op', 'MIXER_UNITS_OP');
+        mdl.setSwitch('#app-mixer_unit-apply_trim', 'MIXER_UNITS_APPLY_TRIM');
+        // FIXME: Add curve and switch
+
+        mdl.setDataURL('#app-mixer_unit-src__edit',
             ['select_single', this.uuid, 'MIXER_UNITS_SRC', this.offset]);
-        MDLHelper.setDataURL('#app-mixer_unit-dst__edit',
+        mdl.setDataURL('#app-mixer_unit-dst__edit',
             ['select_single', this.uuid, 'MIXER_UNITS_DST', this.offset]);
-        MDLHelper.setDataURL('#app-mixer_unit-op__edit',
+        mdl.setDataURL('#app-mixer_unit-op__edit',
             ['select_single', this.uuid, 'MIXER_UNITS_OP', this.offset]);
-    },
 
-    route: function () {
-        MixerUnit.init(this.params);
         showPage('mixer_unit');
-    }
-};
+    };
+})();
 
+MixerUnit.route = function () {
+    'use strict';
+    MixerUnit.init(this.params);
+};

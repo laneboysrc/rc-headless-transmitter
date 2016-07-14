4b111e47bc32b24171a138a708ea053fc30d2f5a
LANE Boys RC <laneboysrc@gmail.com>
2016-07-14 22:34:46 +0800

    Add IndexedDB test code

--- configurator/_includes/database.js
+++ configurator/_includes/database.js
@@ -1,5 +1,97 @@
+var db;
+
 (function () {
     'use strict';
+
+
+    (function () {
+        var request = window.indexedDB.open("HeadlessTX", 2);
+
+        request.onerror = function(event) {
+            console.error(onerror, event);
+        };
+
+        request.onupgradeneeded = function(event) {
+            console.log("onupgradeneeded", event);
+            var db = event.target.result;
+
+            // Create an objectStore for this database
+            var objectStore = db.createObjectStore("data", { keyPath: "uuid" });
+
+            objectStore.transaction.oncomplete = function(event) {
+                console.log("Object store created");
+                addTestData(db);
+            };
+        };
+
+        request.onsuccess = function(event) {
+            console.log("onsuccess", event);
+            db = event.target.result;
+
+            getTestEntry('c91cabaa-44c9-11e6-9bc2-03ac25e30b5b');
+        };
+
+        function addTestData(db) {
+            console.log("addTestData()");
+
+            var transaction = db.transaction("data", "readwrite");
+            transaction.oncomplete = function(event) {
+                console.log("transaction.oncomplete", event);
+
+            };
+
+            var dataObjectStore = transaction.objectStore("data");
+
+
+            var config_version = new Uint32Array(TEST_CONFIG_DATA.buffer, 0, 1)[0];
+            var config = CONFIG_VERSIONS[config_version];
+
+            var entries = [{
+                    data: TEST_CONFIG_DATA.slice(config.MODEL.o, config.MODEL.o + config.MODEL.s),
+                    schema_name: 'MODEL'
+                }, {
+                    data: TEST_CONFIG_DATA.slice(config.TX.o, config.TX.o + config.TX.s),
+                    schema_name: 'TX'
+                }
+            ];
+
+            function addSuccessHandler(event) {
+                console.log("add.onsuccess", event.target.result);
+            }
+
+            for (var i in entries) {
+                var entry = entries[i];
+                console.log(entry);
+                var schema = config[entry.schema_name];
+
+                var uuid_bytes = new Uint8Array(entry.data, schema['UUID'].o, schema['UUID'].s);
+                var uuid = Utils.uuid2string(uuid_bytes);
+
+                var data_to_add = {
+                    data: entry.data,
+                    uuid: uuid,
+                    schema_name: entry.schema_name,
+                    config_version: config_version
+                };
+
+                var request = dataObjectStore.add(data_to_add);
+                request.onsuccess = addSuccessHandler;
+            }
+        }
+
+        function getTestEntry(uuid) {
+            console.log("getTestEntry()");
+            db.transaction('data')
+              .objectStore('data')
+              .get(uuid)
+              .onsuccess = function(event) {
+                    var data = event.target.result;
+                    console.log(data);
+            };
+        }
+
+    })();
+
 
     var Database = function Database(uuid, offset=0) {
         this.data = {};

0339a9d97b76bd70c6ae53fbbcd5870ab4cb8e28
LANE Boys RC <laneboysrc@gmail.com>
2016-07-23 09:25:22 +0800

    Remove libs folder, rename the file as support file for webpack

--- configurator/web-app/webpack.config.js
+++ configurator/web-app/webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const merge = require('webpack-merge');
 const validate = require('webpack-validator');
-const parts = require('./libs/parts');
+const parts = require('./webpack.support');
 
 
 const PATHS = {

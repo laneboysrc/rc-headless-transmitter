555db354f149011aafb34643914285cfe06d8bc1
Werner Lane <laneboysrc@gmail.com>
2016-09-09 15:53:55 +0800

    Pretty-print JSON

--- configurator/web-app/app/modules/about.js
+++ configurator/web-app/app/modules/about.js
@@ -30,7 +30,7 @@ class About {
         cursor.continue();
       }
       else {
-        let json = JSON.stringify(entries);
+        let json = JSON.stringify(entries, null, 2);
 
         let blob = new Blob([json], {type: "application/json"});
         window.saveAs.saveAs(blob, "headless-tx-backup.json");

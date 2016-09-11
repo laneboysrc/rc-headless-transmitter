26b84a8e89fef292595ef9f762a829039f8ae19c
LANE Boys RC <laneboysrc@gmail.com>
2016-09-11 10:25:22 +0800

    Enable caching as it works fine detecting changes due to use of ETAG

--- configurator/esp8266-uart-websocket-bridge/esp8266-uart-websocket-bridge.ino
+++ configurator/esp8266-uart-websocket-bridge/esp8266-uart-websocket-bridge.ino
@@ -176,7 +176,7 @@ void setup() {
     // Serve the configurator app from the SPIFFS file system, but only
     // if the HOST matches our (fake) domain.
     http_server.serveStatic("/", SPIFFS, "/")
-        // .setCacheControl("max-age=86400")
+        .setCacheControl("max-age=86400")
         .setDefaultFile("index.html")
         .setFilter(filterHost);
 

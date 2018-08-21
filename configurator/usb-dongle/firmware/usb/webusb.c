f4d9af791e29edf6152d074edde948b0f63364ac
Werner Lane <laneboysrc@gmail.com>
2018-08-21 09:05:11 +0800

    Change from CDC to WebUSB, enumerating works now

--- configurator/usb-dongle/firmware/usb/webusb.c
+++ configurator/usb-dongle/firmware/usb/webusb.c
@@ -1,202 +1,160 @@
 #include <stdlib.h>
+#include <stdint.h>
+
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/usb/usbd.h>
-#include <libopencm3/usb/cdc.h>
 #include <libopencm3/stm32/f1/nvic.h>
+
+#include <serial_number.h>
 #include <webusb.h>
 
+enum {
+    USB_INTERFACE_WEBUSB,
+    USB_NUMBER_OF_INTERFACES
+};
 
-static const struct usb_device_descriptor dev = {
+#define VENDOR_INTERFACE_CLASS 0xff
+
+#define USB_STRING_LANGUAGE 0
+#define USB_STRING_MANUFACTURER 1
+#define USB_STRING_PRODUCT 2
+#define USB_STRING_SERIAL_NUMBER 3
+
+#define USB_CSCP_NoDeviceClass (0x00)
+#define USB_CONFIG_ATTR_BUSPOWERED 0x80
+#define USB_CONFIG_POWER_MA(mA) ((mA)/2)
+
+#define USB_EP_WEBUSB_IN 0x01
+#define USB_EP_WEBUSB_OUT 0x82
+
+static usbd_device *webusb_device;
+
+uint8_t usbd_control_buffer[128];
+
+
+static const struct usb_device_descriptor device_descriptor = {
     .bLength = USB_DT_DEVICE_SIZE,
     .bDescriptorType = USB_DT_DEVICE,
-    .bcdUSB = 0x0200,
-    .bDeviceClass = USB_CLASS_CDC,
+
+    .bcdUSB = 0x0210,
+    .bDeviceClass = USB_CSCP_NoDeviceClass,
     .bDeviceSubClass = 0,
     .bDeviceProtocol = 0,
+
     .bMaxPacketSize0 = 64,
-    .idVendor = 0x0483,
-    .idProduct = 0x5740,
-    .bcdDevice = 0x0200,
-    .iManufacturer = 1,
-    .iProduct = 2,
-    .iSerialNumber = 3,
-    .bNumConfigurations = 1,
+    .idVendor = 0x6666,
+    .idProduct = 0xeaf1,
+    .bcdDevice = 0x0101,
+
+    .iManufacturer = USB_STRING_MANUFACTURER,
+    .iProduct = USB_STRING_PRODUCT,
+    .iSerialNumber = USB_STRING_SERIAL_NUMBER,
+
+    .bNumConfigurations = 1
 };
 
-/*
- * This notification endpoint isn't implemented. According to CDC spec its
- * optional, but its absence causes a NULL pointer dereference in Linux
- * cdc_acm driver.
- */
-static const struct usb_endpoint_descriptor comm_endp[] = {{
-    .bLength = USB_DT_ENDPOINT_SIZE,
-    .bDescriptorType = USB_DT_ENDPOINT,
-    .bEndpointAddress = 0x83,
-    .bmAttributes = USB_ENDPOINT_ATTR_INTERRUPT,
-    .wMaxPacketSize = 16,
-    .bInterval = 255,
-}};
-
-static const struct usb_endpoint_descriptor data_endp[] = {{
-    .bLength = USB_DT_ENDPOINT_SIZE,
-    .bDescriptorType = USB_DT_ENDPOINT,
-    .bEndpointAddress = 0x01,
-    .bmAttributes = USB_ENDPOINT_ATTR_BULK,
-    .wMaxPacketSize = 64,
-    .bInterval = 1,
-}, {
-    .bLength = USB_DT_ENDPOINT_SIZE,
-    .bDescriptorType = USB_DT_ENDPOINT,
-    .bEndpointAddress = 0x82,
-    .bmAttributes = USB_ENDPOINT_ATTR_BULK,
-    .wMaxPacketSize = 64,
-    .bInterval = 1,
-}};
-
-static const struct {
-    struct usb_cdc_header_descriptor header;
-    struct usb_cdc_call_management_descriptor call_mgmt;
-    struct usb_cdc_acm_descriptor acm;
-    struct usb_cdc_union_descriptor cdc_union;
-} __attribute__((packed)) cdcacm_functional_descriptors = {
-    .header = {
-        .bFunctionLength = sizeof(struct usb_cdc_header_descriptor),
-        .bDescriptorType = CS_INTERFACE,
-        .bDescriptorSubtype = USB_CDC_TYPE_HEADER,
-        .bcdCDC = 0x0110,
+static const struct usb_endpoint_descriptor webusb_endpoints[] = {
+    {
+        .bLength = USB_DT_ENDPOINT_SIZE,
+        .bDescriptorType = USB_DT_ENDPOINT,
+        .bEndpointAddress = 0x01,
+        .bmAttributes = USB_ENDPOINT_ATTR_BULK,
+        .wMaxPacketSize = 64,
+        .bInterval = 5,
     },
-    .call_mgmt = {
-        .bFunctionLength =
-            sizeof(struct usb_cdc_call_management_descriptor),
-        .bDescriptorType = CS_INTERFACE,
-        .bDescriptorSubtype = USB_CDC_TYPE_CALL_MANAGEMENT,
-        .bmCapabilities = 0,
-        .bDataInterface = 1,
-    },
-    .acm = {
-        .bFunctionLength = sizeof(struct usb_cdc_acm_descriptor),
-        .bDescriptorType = CS_INTERFACE,
-        .bDescriptorSubtype = USB_CDC_TYPE_ACM,
-        .bmCapabilities = 0,
-    },
-    .cdc_union = {
-        .bFunctionLength = sizeof(struct usb_cdc_union_descriptor),
-        .bDescriptorType = CS_INTERFACE,
-        .bDescriptorSubtype = USB_CDC_TYPE_UNION,
-        .bControlInterface = 0,
-        .bSubordinateInterface0 = 1,
-     },
+    {
+        .bLength = USB_DT_ENDPOINT_SIZE,
+        .bDescriptorType = USB_DT_ENDPOINT,
+        .bEndpointAddress = 0x82,
+        .bmAttributes = USB_ENDPOINT_ATTR_BULK,
+        .wMaxPacketSize = 64,
+        .bInterval = 5,
+    }
 };
 
-static const struct usb_interface_descriptor comm_iface[] = {{
-    .bLength = USB_DT_INTERFACE_SIZE,
-    .bDescriptorType = USB_DT_INTERFACE,
-    .bInterfaceNumber = 0,
-    .bAlternateSetting = 0,
-    .bNumEndpoints = 1,
-    .bInterfaceClass = USB_CLASS_CDC,
-    .bInterfaceSubClass = USB_CDC_SUBCLASS_ACM,
-    .bInterfaceProtocol = USB_CDC_PROTOCOL_AT,
-    .iInterface = 0,
+static const struct usb_iface_assoc_descriptor webusb_interface_association[] = {
+    {
+        .bLength = USB_DT_INTERFACE_ASSOCIATION_SIZE,
+        .bDescriptorType = USB_DT_INTERFACE_ASSOCIATION,
+        .bFirstInterface = USB_INTERFACE_WEBUSB,
+        .bInterfaceCount = 1,
+        .bFunctionClass = VENDOR_INTERFACE_CLASS,
+        .bFunctionSubClass = 0,
+        .bFunctionProtocol = 0,
+        .iFunction = 0,
+    }
+};
 
-    .endpoint = comm_endp,
+static const struct usb_interface_descriptor webusb_interfaces[] = {
+    {
+        .bLength = USB_DT_INTERFACE_SIZE,
+        .bDescriptorType = USB_DT_INTERFACE,
+        .bInterfaceNumber = USB_INTERFACE_WEBUSB,
+        .bAlternateSetting = 0,
+        .bNumEndpoints = 2,
+        .bInterfaceClass = VENDOR_INTERFACE_CLASS,
+        .bInterfaceSubClass = 0,
+        .bInterfaceProtocol = 0,
+        .iInterface = 0,
 
-    .extra = &cdcacm_functional_descriptors,
-    .extralen = sizeof(cdcacm_functional_descriptors),
-}};
+        .endpoint = webusb_endpoints,
+    }
+};
 
-static const struct usb_interface_descriptor data_iface[] = {{
-    .bLength = USB_DT_INTERFACE_SIZE,
-    .bDescriptorType = USB_DT_INTERFACE,
-    .bInterfaceNumber = 1,
-    .bAlternateSetting = 0,
-    .bNumEndpoints = 2,
-    .bInterfaceClass = USB_CLASS_DATA,
-    .bInterfaceSubClass = 0,
-    .bInterfaceProtocol = 0,
-    .iInterface = 0,
+static const struct usb_interface interfaces[] = {
+    {
+        .num_altsetting = 1,
+        .iface_assoc = webusb_interface_association,
+        .altsetting = webusb_interfaces
+    }
+};
 
-    .endpoint = data_endp,
-}};
-
-static const struct usb_interface ifaces[] = {{
-    .num_altsetting = 1,
-    .altsetting = comm_iface,
-}, {
-    .num_altsetting = 1,
-    .altsetting = data_iface,
-}};
-
-static const struct usb_config_descriptor config = {
+static const struct usb_config_descriptor configuration_descriptor = {
     .bLength = USB_DT_CONFIGURATION_SIZE,
     .bDescriptorType = USB_DT_CONFIGURATION,
     .wTotalLength = 0,
-    .bNumInterfaces = 2,
+    .bNumInterfaces = USB_NUMBER_OF_INTERFACES,
     .bConfigurationValue = 1,
     .iConfiguration = 0,
-    .bmAttributes = 0x80,
-    .bMaxPower = 0x32,
+    .bmAttributes = USB_CONFIG_ATTR_BUSPOWERED,
+    .bMaxPower = USB_CONFIG_POWER_MA(100),
 
-    .interface = ifaces,
+    .interface = interfaces
 };
+
+static char serial_number[9];
 
 static const char *usb_strings[] = {
     "LANE Boys RC",
     "Configurator USB dongle",
-    "12345678",
+    serial_number
 };
 
-static usbd_device *webusb_device;
 
-/* Buffer to be used for control requests. */
-uint8_t usbd_control_buffer[128];
-
-static int cdcacm_control_request(usbd_device *usbd_dev, struct usb_setup_data *req, uint8_t **buf, uint16_t *len, void (**complete)(usbd_device *usbd_dev, struct usb_setup_data *req))
+// ****************************************************************************
+static int webusb_control_request(usbd_device *usbd_dev, struct usb_setup_data *req, uint8_t **buf, uint16_t *len, void (**complete)(usbd_device *usbd_dev, struct usb_setup_data *req))
 {
-    (void)complete;
-    (void)buf;
-    (void)usbd_dev;
+    (void) usbd_dev;
+    (void) buf;
+    (void) len;
+    (void) complete;
 
     switch (req->bRequest) {
-        case USB_CDC_REQ_SET_CONTROL_LINE_STATE: {
-            /*
-             * This Linux cdc_acm driver requires this to be implemented
-             * even though it's optional in the CDC spec, and we don't
-             * advertise it in the ACM functional descriptor.
-             */
-            char local_buf[10];
-            struct usb_cdc_notification *notif = (void *)local_buf;
-
-            /* We echo signals back to host as notification. */
-            notif->bmRequestType = 0xA1;
-            notif->bNotification = USB_CDC_NOTIFY_SERIAL_STATE;
-            notif->wValue = 0;
-            notif->wIndex = 0;
-            notif->wLength = 2;
-            local_buf[8] = req->wValue & 3;
-            local_buf[9] = 0;
-            // usbd_ep_write_packet(0x83, buf, 10);
-            return USBD_REQ_HANDLED;
-            }
-        case USB_CDC_REQ_SET_LINE_CODING:
-            if (*len < sizeof(struct usb_cdc_line_coding))
-                return USBD_REQ_NOTSUPP;
-            return USBD_REQ_HANDLED;
 
         default:
-            break;
+            return USBD_REQ_NOTSUPP;
     }
-    return USBD_REQ_NOTSUPP;
 }
 
-static void cdcacm_data_rx_cb(usbd_device *usbd_dev, uint8_t ep)
+
+// ****************************************************************************
+static void webusb_receive_callback(usbd_device *usbd_dev, uint8_t ep)
 {
     char buf[64];
     int len ;
 
-    (void)ep;
-    (void)usbd_dev;
+    (void) ep;
 
     len = usbd_ep_read_packet(usbd_dev, 0x01, buf, 64);
 
@@ -206,28 +164,27 @@ static void cdcacm_data_rx_cb(usbd_device *usbd_dev, uint8_t ep)
     }
 }
 
-static void cdcacm_set_config(usbd_device *usbd_dev, uint16_t wValue)
+
+// ****************************************************************************
+static void webusb_set_config(usbd_device *usbd_dev, uint16_t wValue)
 {
-    (void)wValue;
-    (void)usbd_dev;
+    (void) wValue;
 
-    usbd_ep_setup(usbd_dev, 0x01, USB_ENDPOINT_ATTR_BULK, 64, cdcacm_data_rx_cb);
+    usbd_ep_setup(usbd_dev, 0x01, USB_ENDPOINT_ATTR_BULK, 64, webusb_receive_callback);
     usbd_ep_setup(usbd_dev, 0x82, USB_ENDPOINT_ATTR_BULK, 64, NULL);
-    usbd_ep_setup(usbd_dev, 0x83, USB_ENDPOINT_ATTR_INTERRUPT, 16, NULL);
-
-    usbd_register_control_callback(
-                usbd_dev,
-                USB_REQ_TYPE_CLASS | USB_REQ_TYPE_INTERFACE,
-                USB_REQ_TYPE_TYPE | USB_REQ_TYPE_RECIPIENT,
-                cdcacm_control_request);
+    usbd_register_control_callback(usbd_dev, USB_REQ_TYPE_CLASS | USB_REQ_TYPE_INTERFACE, USB_REQ_TYPE_TYPE | USB_REQ_TYPE_RECIPIENT, webusb_control_request);
 }
 
+
+// ****************************************************************************
 void WEBUSB_poll(void)
 {
     usbd_poll(webusb_device);
     // nvic_clear_pending_irq(NVIC_USB_LP_CAN_RX0_IRQ);
 }
 
+
+// ****************************************************************************
 void WEBUSB_init(void)
 {
     volatile uint32_t i;
@@ -235,8 +192,10 @@ void WEBUSB_init(void)
     gpio_set(GPIOC, GPIO11);
     gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO11);
 
-    webusb_device = usbd_init(&st_usbfs_v1_usb_driver, &dev, &config, usb_strings, 3, usbd_control_buffer, sizeof(usbd_control_buffer));
-    usbd_register_set_config_callback(webusb_device, cdcacm_set_config);
+    SERIAL_NUMBER_get(serial_number);
+
+    webusb_device = usbd_init(&st_usbfs_v1_usb_driver, &device_descriptor, &configuration_descriptor, usb_strings, 3, usbd_control_buffer, sizeof(usbd_control_buffer));
+    usbd_register_set_config_callback(webusb_device, webusb_set_config);
 
     for (i = 0; i < 0x800000; i++) {
         __asm__("nop");
